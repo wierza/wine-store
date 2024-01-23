34dce90ac75c88259a3f8cb29a3061d3e2ea88ae
Dawid <dawid@wierzycki.pl>
2024-01-23 22:55:39 +0100

    Add products endpoints

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Product } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
     constructor(private prismaService: PrismaService) {}
+    
+    public getAll(): Promise<Product[]> {
+      return this.prismaService.product.findMany();
+    }
+    
+    public getById(id: Product['id']): Promise<Product | null> {
+      return this.prismaService.product.findUnique({
+        where: { id },
+      });
+    }
 }
